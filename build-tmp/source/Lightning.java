619b4bcb536d9e6a04cacd2faff96a21b447c547
giangd <giangd@rocketmail.com>
2015-09-03 21:10:16 -0700

    added lightning

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Lightning extends PApplet {
+
 //now that i realize it i couldve just done 4 directions >.>
 int xRange = 20;//for up and down
 int yRange = 10;
@@ -22,7 +38,7 @@ int counter = 0;//to make sparks at intervals
   7 6 5
  */
 
-void setup() {
+public void setup() {
   size(800, 650);
   fill(0,0,0);
   background(0, 0, 0);
@@ -32,16 +48,16 @@ void setup() {
   // }
 }
 
-void draw() {
+public void draw() {
 }
 
-void mouseMoved() {
+public void mouseMoved() {
  background(0, 0, 0);
  counter++;
   if (counter > 1) {
     for (int i=0; i<bolts.length; i++) {
       // stroke(200+(int)(Math.random()*55),255-(int)(Math.random()*55),0);
-      stroke(200,255,0,100+(int)(Math.random()*150));
+      stroke(200,255,0,100+(int)(Math.random()*100));
       bolts[i] = new LightningBolt(mouseX, mouseY, i);
       bolts[i].show();
       counter = 0;
@@ -67,7 +83,7 @@ class LightningBolt {
     dir = tempDir;
   }
 
-  void show() {
+  public void show() {
     if (dir == 6) {
       while (endY < height) {
         endX = startX + (int)(Math.random()*xRange) - xRange/2;
@@ -132,6 +148,15 @@ class LightningBolt {
         startX = endX;
         startY = endY;
       }
+    }
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Lightning" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
     }
   }
 }
